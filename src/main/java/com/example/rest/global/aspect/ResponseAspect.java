a87a73e8950f07de5e04c98906fc749d2c3f4d76
jxxngin <wwd0203@daum.net>
2025-02-11 17:47:09 +0900

    [feat] response add in aspect

--- src/main/java/com/example/rest/global/aspect/ResponseAspect.java
+++ src/main/java/com/example/rest/global/aspect/ResponseAspect.java
@@ -1,6 +1,8 @@
 package com.example.rest.global.aspect;
 
 import com.example.rest.global.dto.RsData;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,7 +10,10 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class ResponseAspect {
+
+    private final HttpServletResponse response;
 
     @Around("""
             (
@@ -31,16 +36,14 @@ public class ResponseAspect {
                 @annotation(org.springframework.web.bind.annotation.ResponseBody)
             """)
     public Object test(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("pre");  // 전처리
-
         Object rst = joinPoint.proceed();   // 실제 수행 메서드
 
         if (rst instanceof RsData rsData) {
             String msg = rsData.getMsg();
-            System.out.println("msg : " + msg);
-        }
 
-        System.out.println("post"); // 후처리
+            // 응답 코드를 설정
+            response.setStatus(201);
+        }
 
         return rst;
     }
