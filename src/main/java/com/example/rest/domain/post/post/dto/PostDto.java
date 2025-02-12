dc5086c534df32278d1357e25652e904782f9db4
jxxngin <wwd0203@daum.net>
2025-02-12 13:03:18 +0900

    [feat] author id, name

--- src/main/java/com/example/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/example/rest/domain/post/post/dto/PostDto.java
@@ -1,5 +1,6 @@
 package com.example.rest.domain.post.post.dto;
 
+import com.example.rest.domain.member.member.dto.MemberDto;
 import com.example.rest.domain.post.post.entity.Post;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -18,12 +19,18 @@ public class PostDto {
     private String title;
     private String content;
 
+    private long authorId;
+    private String authorName;
+
     public PostDto(Post post) {
         this.id = post.getId();
         this.createdDate = post.getCreatedDate();
         this.modifiedDate = post.getModifiedDate();
         this.title = post.getTitle();
         this.content = post.getContent();
+
+        this.authorId = post.getAuthor().getId();
+        this.authorName = post.getAuthor().getNickname();
     }
 
 }
